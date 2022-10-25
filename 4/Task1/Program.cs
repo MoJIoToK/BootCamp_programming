3892596269e7a9b35f0941291aaf9797fcad1090
Nick <mojiotok167@gmail.com>
2022-10-25 19:43:20 +0300

    добавил четвертое и пятое занятие

---
+++ 4/Task1/Program.cs
@@ -0,0 +1,28 @@
+﻿//описание см. в файле README.md
+
+Console.WriteLine("Введите количество элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+// Заполнение массива
+int[] array = new int[n];
+for (int i = 0; i < n; i++)
+{
+    Console.Write("Введите число: ");
+    array[i] = Convert.ToInt32(Console.ReadLine());
+}
+Console.WriteLine();
+Console.WriteLine("Начальный массив: ["+ string.Join(", ", array)+"]");
+//Сортировка
+for (int i = 0; i < n - 1; i++)
+{
+    int min_index = i;
+    for (int j = i + 1; j < n; j++)
+    {
+        if (array[j] < array[min_index])
+            min_index = j;
+    }
+    int temp;
+    temp = array[min_index];
+    array[min_index] = array[i];
+    array[i] = temp; 
+}
+Console.WriteLine("Конечный массив: ["+ string.Join(", ", array)+"]");
---
+++ 4/Task2/Program.cs
@@ -0,0 +1,28 @@
+﻿/*
+привет = (120) + (117) + (110) + (108) + (109) + (119)
+Привет = (90) + (117) + (110) + (108) + (109) + (119)
+Номера кодировок в UTF-8 условное.
+
+*/
+
+string[] array = new string[5];
+for (int i = 0; i < 5; i++)
+{
+    array[i] = Console.ReadLine();
+}
+
+Console.Write("[" + string.Join(", ", array) + "]");
+for (int i = 0; i < 4; i++)
+{
+    int MinIndex = i;
+    for (int j = i + 1; j < 5; j++)
+    {
+        if (array[j].Length < array[MinIndex].Length)
+            MinIndex = j;
+    }
+    string temp;
+    temp = array[MinIndex];
+    array[MinIndex] = array[i];
+    array[i] = temp;
+}
+Console.WriteLine("Конечный массив: [" + string.Join(", ", array) + "]");
