fa878a73e745da18030c94cb6c06b5636299b67e
Nick <mojiotok167@gmail.com>
2022-10-25 20:43:43 +0300

    Обновил пятое занятие

--- 5/Task1/Program.cs
+++ 5/Task1/Program.cs
@@ -1,11 +1,26 @@
-﻿/* 
-Сортировка пузырьком
-[3, 1, 5, 0, 7, 9, 8] - начальный массив
-1.[1, 3, 5, 0, 7, 9, 8]
-2.[1, 3, 0, 5, 7, 9, 8]
-3.[1, 0, 3, 5, 7, 9, 8]
-4.[0, 1, 3, 5, 7, 9, 8]
-5.[0, 1, 3, 5, 7, 8, 9]
-6.[0, 1, 3, 5, 7, 8, 9]
-7.[0, 1, 3, 5, 7, 8, 9]
-*/
+﻿
+Console.WriteLine("Введите количество элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+for (int i = 0; i < n; i++)
+{
+    Console.Write("Введите значения массива: ");
+    array[i] = Convert.ToInt32(Console.ReadLine());
+}
+Console.WriteLine("Начальный массив: [" + string.Join(", ", array) + "]");
+Console.WriteLine();
+
+for (int i = 0; i < n; i++)
+{
+    int temp = 0;
+    for (int j = 0; j < n - 1; j++)
+    {
+        if (array[j] > array[j + 1])
+        {
+            temp = array[j];
+            array[j] = array[j + 1];
+            array[j + 1] = temp;
+        }
+    }
+    Console.WriteLine(i + "[" + string.Join(", ", array) + "]");
+}
