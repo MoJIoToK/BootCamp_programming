2034cf2f22bad9d706bad1c0f8d03d90b927b4d5
Nick <mojiotok167@gmail.com>
2022-10-30 20:12:41 +0300

    добавил 10 занятие

---
+++ 10/Counting Sort/Program.cs
@@ -0,0 +1,56 @@
+﻿//сортировка подсчетом
+
+int[] array = {-5, -10, -50, 0, 2, 5, 1, 3, 1, 20, 10};
+int[] sortedArray = CountingSortExtended(array);
+
+//CountingSort(array);
+
+Console.WriteLine(string.Join(", ", sortedArray));
+
+void CountingSort(int[] inputArray)
+{
+    int[] counters = new int[10]; //массив повторений
+
+    for (int i = 0; i < inputArray.Length; i++)
+    {
+        counters[inputArray[i]]++;
+        // ourNumber = inputArray[i];
+        // counters[ourNumber]++;
+    }
+
+    int index = 0;
+    for (int i = 0; i < counters.Length; i++) // обход массива повторений
+    {
+        for (int j = 0; j < counters[i]; j++) //цикл повторяется столько раз, сколько повторений числа в исходном массиве 
+        {
+            inputArray[index] = i;
+            index++;
+        }
+    }
+}
+
+int[] CountingSortExtended(int[] inputArray)
+{
+    int max = inputArray.Max();
+    int min = inputArray.Min();
+
+    int offset = -min; //смещение
+    int[] sortedArray = new int[inputArray.Length];
+    int[] counters = new int[max + offset + 1];
+
+    for (int i = 0; i < inputArray.Length; i++)
+    {
+        counters[inputArray[i] + offset]++;
+    }
+
+    int index = 0;
+    for (int i = 0; i < counters.Length; i++)
+    {
+        for (int j = 0; j < counters[i]; j++)
+        {
+            sortedArray[index] = i - offset;
+            index++;
+        }
+    }
+    return sortedArray;
+}
